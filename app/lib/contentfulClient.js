81af6cec0c7b5597b9a24a0abc6942ed98f5e156
ranaadeel15911 <ra0511083@gmail.com>
2023-10-25 09:38:03 +0500

    no msg

---
+++ app/lib/contentfulClient.js
@@ -0,0 +1,9 @@
+const contentful = require('contentful')
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: process.env.SPACE_ID,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken:process.env.CONTENT_DELIVERY ,
+})
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token
+export default client
